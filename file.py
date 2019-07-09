f4cfbf6e50e5f32642a57ba8dd6c751d164ad4e5
aadilraza339 <47937273+aadilraza339@users.noreply.github.com>
2019-07-09 08:56:27 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,8 @@
+1 => cd Desktop
+2 => mkdir (folder name whatever you want to create)
+2 => npm init
+3 => npm install express --save 
+4 => npm install fs --save
+5 => npm install nodemon --save
+
+now your package is created and you can write your code  
