09e2b12584f8383bb90f2f1b894911d0a855cda0
aadilraza339 <47937273+aadilraza339@users.noreply.github.com>
2019-08-06 22:29:48 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,10 @@
 1 => cd Desktop
 2 => mkdir (folder name whatever you want to create)
-2 => npm init
-3 => npm install express --save 
-4 => npm install fs --save
-5 => npm install nodemon --save
-
+3 => npm init
+4 => npm install express --save 
+5 => npm install fs --save
+6 => npm install nodemon --save
+7 => touch and your file name 
 now your package is created and you can write your code  
+
+
